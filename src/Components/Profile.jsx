fa109a720bafbee4f8eb1a0fe77b7bd7450bc7ca
Student_GA111 <egen03@gmail.com>
2020-07-25 15:16:42 -0400

    sized image

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -7,7 +7,9 @@ const Profile = () => {
             <div className="row">
           
                 <div className="col-sm">
-                <img src="..." class="rounded float-left" alt="..." />
+                {/* <div> */}
+                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/burger.jpeg")} class="rounded float-left" alt="Burger" height="300px" width="300px" />
+                {/* </div> */}
                 </div>
           
                 <div className="col-sm">
