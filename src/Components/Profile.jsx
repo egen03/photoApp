0d1bfc335bf8a82de1c4430dc43997ff03f50689
Student_GA111 <egen03@gmail.com>
2020-07-25 14:42:07 -0400

    added another row to grid

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -3,20 +3,40 @@ import React from 'react';
 const Profile = () => {
     return (
         <div className="container">
-        <div className="row">
+        
+            <div className="row">
           
-          <div className="col-sm">
-            One of three columns
-          </div>
+                <div className="col-sm">
+                <img src="..." className="rounded float-left" alt="..." />
+                </div>
           
-          <div className="col-sm">
-            One of three columns
-          </div>
+                <div className="col-sm">
+                    One of three columns
+                </div>
           
-          <div className="col-sm">
-            One of three columns
-          </div>
-        </div>
+                <div className="col-sm">
+                    One of three columns
+                </div>
+
+            </div>
+
+            <div className="row">
+          
+                <div className="col-sm">
+                    One of three columns
+                </div>
+          
+                <div className="col-sm">
+                    One of three columns
+                </div>
+          
+                <div className="col-sm">
+                    One of three columns
+                </div>
+
+  
+            </div>
+
       </div>
     )
 } 
