6b4aeb6ea5b827dbc0082bf3ffc2657e48b9f352
Student_GA111 <egen03@gmail.com>
2020-07-25 13:22:13 -0400

    replaced a tags with link tags

--- src/App.js
+++ src/App.js
@@ -17,10 +17,9 @@ function App() {
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="#">Pricing</a>
-      <a class="nav-item nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+      <Link to="/home" className="nav-link" >Home</Link>
+      <Link to="/profile" className="nav-link" >Profiles</Link>
+      <Link to="/bookmark" className="nav-link" >Bookmarks</Link>
     </div>
   </div>
 </nav>
