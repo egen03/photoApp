88edf37de9f7b3db4fecaa4fe86c930013a90309
Student_GA111 <egen03@gmail.com>
2020-07-25 13:17:26 -0400

    removed react router nav documentation

--- src/App.js
+++ src/App.js
@@ -10,19 +10,7 @@ export default function App() {
   return (
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
+        
 
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
