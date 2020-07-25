eebcdf0249b50313ec89f384b154f0748b40b9cb
Student_GA111 <egen03@gmail.com>
2020-07-25 13:33:35 -0400

    imported react to home component

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ function App() {
             <Profile />
           </Route>
           <Route path="/bookmark">
-            <Bookmarks />
+            <Bookmark />
           </Route>
         </Switch>
       </div>
