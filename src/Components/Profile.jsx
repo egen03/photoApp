6f1529464114aa010f910b1f106d449282989b0d
Student_GA111 <egen03@gmail.com>
2020-07-27 13:49:44 -0400

    imported style file for profile page

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
+import './Styles/profile.css';
 
 const Profile = () => {
     return(
 <div className="profile">
-<div class="container">
+
+    <div class="container">
   
   <div>
       <h1>Top Bloggers</h1>
       <p>These users have the most likes and followers </p>
   </div> 
 
-  <div class="row">
+  <div className="row">
 
-    <div class="col-md">
+    <div className="col-md">
     <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/person1.jpeg")} class="rounded float-left" alt="Makayla" height="250px" width="250px" />
     </div>
 
-    <div class="col-md">
+    <div className="col-md">
     <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/person2.jpeg")} class="rounded float-left" alt="Erin" height="250px" width="250px" />
     </div>
 
-    <div class="col-md">
+    <div className="col-md">
     <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/person3.jpeg")} class="rounded float-left" alt="Mike" height="250px" width="250px" />
     </div>
 
-  </div>
+  </div> 
 
 </div>
 
