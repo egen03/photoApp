270aa25de414e573601dc3b7235e648f18734149
Student_GA111 <egen03@gmail.com>
2020-07-27 15:30:29 -0400

    added 1st bookmarked pic

--- src/Components/Bookmark.jsx
+++ src/Components/Bookmark.jsx
@@ -5,7 +5,7 @@ const Bookmark = () => {
         <div class="container">
   <div class="row">
     <div class="col-sm">
-      One of three columns
+    <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/mussels.jpeg")} class="rounded float-left" alt="Mussels" height="250px" width="250px" />
     </div>
     <div class="col-sm">
       One of three columns
