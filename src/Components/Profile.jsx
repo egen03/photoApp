ea717788d64c544af879d28314c655fdf3fead24
Student_GA111 <egen03@gmail.com>
2020-07-27 11:49:37 -0400

    added 3rd profile pic

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -15,7 +15,7 @@ const Profile = () => {
     </div>
 
     <div class="col-md">
-      One of three columns
+    <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/person3.jpeg")} class="rounded float-left" alt="Mike" height="250px" width="250px" />
     </div>
 
   </div>
