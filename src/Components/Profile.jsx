b3012fc483293e22f0ed3cfbd1970c89c12a7a00
Student_GA111 <egen03@gmail.com>
2020-07-27 10:19:43 -0400

    created styles folder

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,46 +1,11 @@
 import React from 'react';
 
 const Profile = () => {
-    return (
-        <div className="container">
-        
-            <div className="row">
-          
-                <div className="col-sm">
-                {/* <div> */}
-                    <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/burger.jpeg")} class="rounded float-left" alt="Burger" height="250px" width="250px" />
-                {/* </div> */}
-                </div>
-          
-                <div className="col-sm">
-                    <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/mussels.jpeg")} class="rounded float-left" alt="Mussels" height="250px" width="250px" />
-                </div>
-          
-                <div className="col-sm">
-                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/pasta.jpeg")} class="rounded float-left" alt="Pasta" height="250px" width="250px" />
-                </div>
-
-            </div>
-
-            <div className="row">
-          
-                <div className="col-sm">
-                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/salad.jpeg")} class="rounded float-left" alt="Salad" height="250px" width="250px" />
-                </div>
-          
-                <div className="col-sm">
-                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/steak.jpeg")} class="rounded float-left" alt="Steak" height="250px" width="250px" />
-                </div>
-          
-                <div className="col-sm">
-                <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/sweets.jpeg")} class="rounded float-left" alt="Sweets" height="250px" width="250px" />
-                </div>
-
-  
-            </div>
-
-      </div>
+    return(
+        <div>
+            <h1>Hello</h1>
+        </div>
     )
-} 
+}
 
 export default Profile;
