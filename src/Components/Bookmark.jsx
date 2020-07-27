0cc46798eea983a7b785311eabd445cff267ec00
Student_GA111 <egen03@gmail.com>
2020-07-27 15:28:48 -0400

    imported grid documentation to bookmark

--- src/Components/Bookmark.jsx
+++ src/Components/Bookmark.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const Bookmark = () => {
-    // return ()
+    return (
+        <div class="container">
+  <div class="row">
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+    <div class="col-sm">
+      One of three columns
+    </div>
+  </div>
+</div>
+    )
 }
 
 export default Bookmark;
