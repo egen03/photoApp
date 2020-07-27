b35ac96b7bb32607806c56f1adb63db1d47f0180
Student_GA111 <egen03@gmail.com>
2020-07-27 11:47:01 -0400

    added 1st profile pic

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -7,7 +7,7 @@ const Profile = () => {
   <div class="row">
 
     <div class="col-md">
-      One of three columns
+    <img src={require("/Users/student_ga111/Desktop/Cassini /mod2/wk3/homework/photoApp/photoapp/src/Components/Assets/Images/person1.jpeg")} class="rounded float-left" alt="Makayla" height="250px" width="250px" />
     </div>
 
     <div class="col-md">
@@ -17,7 +17,7 @@ const Profile = () => {
     <div class="col-md">
       One of three columns
     </div>
-    
+
   </div>
 
 </div>
